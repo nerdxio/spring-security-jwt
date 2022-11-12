28bbf37082a3f50f2594eca53b1e92d6322ecf27
Hassan Refaat <hassan.refaat.dev@gmail.com>
2022-11-12 08:39:40 +0200

    auth and auth

--- src/main/java/com/nerd/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/nerd/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -38,5 +40,10 @@ public class SpringSecurityJwtApplication {
             userService.addRoleToUser("abdo","ROLE_ADMIN");
             userService.addRoleToUser("abdo","ROLE_USER");
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
